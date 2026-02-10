3f4289b5b7b16947325dd8512a7811437878c6a0
Marius Bär <marius.baer@proton.me>
2026-02-10 09:01:30 +0000

    Add differential QAM decoder

---
+++ custom_blocks/differential_qam_decoder.py
@@ -0,0 +1,31 @@
+import numpy as np
+from gnuradio import gr
+from enum import Enum
+
+class qam(Enum):
+    QAM4 = 0
+    QAM16 = 1
+    QAM64 = 2
+    QAM256 = 3
+
+class differential_qam_decoder(gr.sync_block):  # other base classes are basic_block, decim_block, interp_block
+    """"""
+
+    def __init__(self, order=qam.QAM4):
+        gr.sync_block.__init__(
+            self,
+            name='Differential QAM Decoder',   # will show up in GRC
+            in_sig=[np.uint8],
+            out_sig=[np.uint8]
+        )
+        self.order = order
+        self.mask = np.uint8(3 << (2 * self.order.value))
+        self.modulo = 2**((self.order.value+1)*2)
+        self.prev_symbol = np.uint8(0)
+
+    def work(self, input_items, output_items):
+        for i in range(len(input_items[0])):
+            with np.errstate(over='ignore'): # Underflow might happen (suppress warning since it's intentional)
+                output_items[0][i] = (np.uint8(input_items[0][i]) - (self.prev_symbol & self.mask)) % self.modulo
+            self.prev_symbol = np.uint8(input_items[0][i])
+        return len(output_items[0])
---
+++ custom_blocks/test_differential_qam_decoder.py
@@ -0,0 +1,79 @@
+import unittest
+import numpy as np
+from gnuradio import gr, gr_unittest, blocks
+
+from differential_qam_decoder import differential_qam_decoder, qam
+
+
+class test_differential_qam_decoder(gr_unittest.TestCase):
+
+    def setUp(self):
+        self.tb = gr.top_block()
+
+    def tearDown(self):
+        self.tb = None
+
+    def generate_qam_test_data(self, order):
+        quadrant_diff = np.array(range(4), dtype=np.uint8).repeat(2**(order.value*2))
+        quadrant = quadrant_diff.cumsum() % 4
+        symbol_part_in_quadrant = np.array(list(range(2**(order.value*2))) * 4, dtype=np.uint8)
+        src_data = quadrant << order.value*2 | symbol_part_in_quadrant
+        expected = quadrant_diff << order.value*2 | symbol_part_in_quadrant
+        return (src_data, expected,)
+
+    def test_qam4(self):
+        (src_data, expected) = self.generate_qam_test_data(qam.QAM4)
+
+        src = blocks.vector_source_b(src_data.tolist(), False)
+        enc = differential_qam_decoder(order=qam.QAM4)
+        sink = blocks.vector_sink_b()
+
+        self.tb.connect(src, enc, sink)
+        self.tb.run()
+
+        result = sink.data()
+        print(src_data)
+        self.assertEqual(list(result), list(expected))
+
+    def test_qam16(self):
+        (src_data, expected) = self.generate_qam_test_data(qam.QAM16)
+
+        src = blocks.vector_source_b(src_data.tolist(), False)
+        enc = differential_qam_decoder(order=qam.QAM16)
+        sink = blocks.vector_sink_b()
+
+        self.tb.connect(src, enc, sink)
+        self.tb.run()
+
+        result = sink.data()
+        self.assertEqual(list(result), list(expected))
+
+    def test_qam64(self):
+        (src_data, expected) = self.generate_qam_test_data(qam.QAM64)
+
+        src = blocks.vector_source_b(src_data.tolist(), False)
+        enc = differential_qam_decoder(order=qam.QAM64)
+        sink = blocks.vector_sink_b()
+
+        self.tb.connect(src, enc, sink)
+        self.tb.run()
+
+        result = sink.data()
+        self.assertEqual(list(result), list(expected))
+
+    def test_qam256(self):
+        (src_data, expected) = self.generate_qam_test_data(qam.QAM256)
+
+        src = blocks.vector_source_b(src_data.tolist(), False)
+        enc = differential_qam_decoder(order=qam.QAM256)
+        sink = blocks.vector_sink_b()
+
+        self.tb.connect(src, enc, sink)
+        self.tb.run()
+
+        result = sink.data()
+        self.assertEqual(list(result), list(expected))
+
+
+if __name__ == '__main__':
+    gr_unittest.run(test_differential_qam_decoder)
