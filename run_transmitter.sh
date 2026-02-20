ba554608a54b4f9c8ab194a9053f936829d28236
Marius Bär <marius.baer@proton.me>
2026-02-20 20:34:22 +0000

    Add run_transmitter script

---
+++ run_transmitter.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+DIR_OF_THIS_SCRIPT=$(dirname "$(realpath $0)")
+
+$DIR_OF_THIS_SCRIPT/compile_flowgraphs.sh
+
+export GRC_BLOCKS_PATH=$DIR_OF_THIS_SCRIPT/custom_blocks
+export PYTHONPATH=$GRC_BLOCKS_PATH
+
+python3 $DIR_OF_THIS_SCRIPT/transmitter.py
+
