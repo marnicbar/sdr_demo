a2c72ccb188a0ce24dd077902aa5b3997669b8a3
Marius Bär <marius.baer@proton.me>
2026-02-21 10:29:13 +0000

    Update run and compile scripts to work only compile either the transmitter or the receiver and move general task to common_rpi_setup.yml

--- compile_flowgraphs.sh
+++ compile_flowgraphs.sh
@@ -4,5 +4,9 @@ DIR_OF_THIS_SCRIPT=$(dirname "$(realpath $0)")
 export GRC_BLOCKS_PATH=$DIR_OF_THIS_SCRIPT/custom_blocks
 export PYTHONPATH=$GRC_BLOCKS_PATH
 
-grcc transmitter.grc
-grcc receiver.grc
+if [ -z "$1" ]; then
+	echo "Usage: $0 <flowgraph.grc>" >&2
+	exit 1
+fi
+
+grcc "$1"
--- run_receiver.sh
+++ run_receiver.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 DIR_OF_THIS_SCRIPT=$(dirname "$(realpath $0)")
 
-$DIR_OF_THIS_SCRIPT/compile_flowgraphs.sh
+$DIR_OF_THIS_SCRIPT/compile_flowgraphs.sh receiver.grc
 
 export GRC_BLOCKS_PATH=$DIR_OF_THIS_SCRIPT/custom_blocks
 export PYTHONPATH=$GRC_BLOCKS_PATH
 
 python3 $DIR_OF_THIS_SCRIPT/receiver.py
-
