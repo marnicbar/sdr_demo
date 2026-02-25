7227c09007b4d0559622c53aa5deef53afacadfe
Marius Bär <marius.baer@proton.me>
2026-02-25 10:01:20 +0000

    Add callback to short/byte convertion blocks to make them runtime adjustable

--- custom_blocks/byte_to_short.py
+++ custom_blocks/byte_to_short.py
@@ -1,4 +1,5 @@
 import numpy as np
+import threading
 from gnuradio import gr
 
 
@@ -10,11 +11,20 @@ class byte_to_short(gr.sync_block):
             in_sig=[np.uint8],
             out_sig=[np.int16]
         )
+        self._lock = threading.Lock()
         if (n_bits < 1 or n_bits > 8):
             raise ValueError(f"n_bits must be in range 1..8 (was {n_bits})")
-        self.n_bits = n_bits
+        self.n_bits = int(n_bits)
+
+    def set_n_bits(self, n_bits):
+        n_bits = int(n_bits)
+        if (n_bits < 1 or n_bits > 8):
+            raise ValueError(f"n_bits must be in range 1..8 (was {n_bits})")
+        with self._lock:
+            self.n_bits = n_bits
 
     def work(self, input_items, output_items):
-        output_items[0][:] = (input_items[0].astype(np.uint16) << (16 - self.n_bits)).astype(np.int16)
+        with self._lock:
+            n_bits = self.n_bits
+        output_items[0][:] = (input_items[0].astype(np.uint16) << (16 - n_bits)).astype(np.int16)
         return len(output_items[0])
-
--- custom_blocks/short_to_byte.py
+++ custom_blocks/short_to_byte.py
@@ -1,4 +1,5 @@
 import numpy as np
+import threading
 from gnuradio import gr
 
 
@@ -10,14 +11,23 @@ class short_to_byte(gr.sync_block):
             in_sig=[np.int16],
             out_sig=[np.uint8]
         )
+        self._lock = threading.Lock()
         if (n_bits < 1 or n_bits > 8):
             raise ValueError(f"n_bits must be in range 1..8 (was {n_bits})")
-        self.n_bits = n_bits
+        self.n_bits = int(n_bits)
+
+    def set_n_bits(self, n_bits):
+        n_bits = int(n_bits)
+        if (n_bits < 1 or n_bits > 8):
+            raise ValueError(f"n_bits must be in range 1..8 (was {n_bits})")
+        with self._lock:
+            self.n_bits = n_bits
 
     def work(self, input_items, output_items):
-        shift = 16 - self.n_bits
-        output_items[0][:] = (
-            ((input_items[0].astype(np.int16).clip(-2**15, 2**15 - 2**(shift-1) - 1) + (1 << (shift - 1))) >> shift)
-            .astype(np.uint8)
-        )
+        with self._lock:
+            shift = 16 - self.n_bits
+            output_items[0][:] = (
+                ((input_items[0].astype(np.int16).clip(-2**15, 2**15 - 2**(shift-1) - 1) + (1 << (shift - 1))) >> shift)
+                .astype(np.uint8)
+            )
         return len(output_items[0])
